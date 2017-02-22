704dab3ef0f1dfc4dd4fad3a88f82d309e322b2c
Rico <richardthomaswhitfield@gmail.com>
2017-02-22 11:03:54 -0600

    TestProduct setup

--- bangazon_website/bang_app/tests/TestProduct.py
+++ bangazon_website/bang_app/tests/TestProduct.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from bang_app.models import Customer, Product
+
+class ProductTestCase(TestCase):
+
+	# def setUp(self):
+	# 	Product.objects.create()
+	
+	def test_user_can_create_product():
+

ef6b7f0a233637dba31ec23099715c14872e26d3
Abby Moore <abbyleighanne@gmail.com>
2017-02-22 13:53:27 -0600

    customer model

--- bangazon_website/bang_app/models/Customer.py
+++ bangazon_website/bang_app/models/Customer.py
@@ -8,23 +8,23 @@ import datetime
 
 
 
-class Customer():
+class Customer(models.Model):
 	"""
 	Purpose: Create a Customer
 	Author: Abby
-	Methods: str
+	Methods: str - returns first and last name
 		
 	"""
-		user = models.OneToOneField(User, on_delete=models.CASCADE) 
-		date = models.DateTimeField(default=datetime.now, blank=True)
-		street_address = models.CharField(max_length=70)
-		city = models.CharField(max_length=70)
-		state = models.CharField(max_length=70)
-		zip_code = models.CharField(max_length=10)
+	user = models.OneToOneField(User, on_delete=models.CASCADE) 
+	date = models.DateTimeField(default=datetime.now, blank=True)
+	street_address = models.CharField(max_length=70)
+	city = models.CharField(max_length=70)
+	state = models.CharField(max_length=70)
+	zip_code = models.CharField(max_length=10)
 
 
-		def __str__(self):
-		return "{} {}".format(self.user.first_name, self.user.last_name)
+	def __str__(self):
+	return "{} {}".format(self.user.first_name, self.user.last_name)
 
 	
 

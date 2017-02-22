3a96d993074ef6ee4a2aa244a7e37b1db685bb40
Rico <richardthomaswhitfield@gmail.com>
2017-02-22 13:45:01 -0600

    removed superfluos test file

--- bangazon_website/bang_app/tests/test_product.py
+++ bangazon_website/bang_app/tests/test_product.py
@@ -5,7 +5,29 @@ sys.path.append('../')
 
 class TestProduct(TestCase):
 
+	'''
+		Purpose:
+			This class tests that matters realted to Product
+
+		Methods:
+			def test_product_can_be_created(self):
+			def test_customer_can_create_product(self):
+			def test_product_has_type(self):
+				
+		Author:
+			@rtwhitfield84
+	'''
+
 	
 	def test_product_can_be_created(self):
 		self.ball = Product.objects.create("ball", "1.99", "3", 1, 1)
 		self.assertIsInstance(self.ball, Product)
+
+	def test_customer_can_create_product(self):
+		self.customer = Customer()
+		self.ball = Product.objects.create("ball", "1.99", "3", 1, 1)
+		self.customer.create_product(self.ball)
+
+	def test_product_has_type(self):
+		self.ball = Product.objects.create("ball", "1.99", "3", 1, 1)
+		self.customer.create_product(self.ball)

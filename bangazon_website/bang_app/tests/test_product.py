88b21d5b2422c5434491fa06c6096b1b2bbfdba1
Rico <richardthomaswhitfield@gmail.com>
2017-02-23 15:47:33 -0600

    updated product model test

--- bangazon_website/bang_app/tests/test_product.py
+++ bangazon_website/bang_app/tests/test_product.py
@@ -20,16 +20,14 @@ class TestProduct(TestCase):
 	
 	def test_product_can_be_created(self):
 
-		self.ball = Product("ball", 1.99, "It's round", 3, 1, None)
+		self.ball = Product("ball", 1.99, "It's round", 3, None, None)
 		self.assertIsInstance(self.ball, Product)
 	
 	def test_product_fields_contain_correct_values(self):
 
-		ball = Product(None,"ball", 1.99, "It's round", 3, 3, None)
+		ball = Product(None,"ball", 1.99, "It's round", 3, None, None)
 
 		self.assertEqual(ball.name, "ball")
 		self.assertEqual(ball.price, 1.99)
 		self.assertEqual(ball.description, "It's round")
 		self.assertEqual(ball.quantity, 3)
-		self.assertEqual(ball.product_type, None)
-		self.assertEqual(ball.seller, None)

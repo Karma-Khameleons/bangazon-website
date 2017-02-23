4df26023eda6cccf1fc9230e4873074369f91f8c
Rico <richardthomaswhitfield@gmail.com>
2017-02-23 17:12:27 -0600

    renamed product view test to refelct test

--- bangazon_website/bang_app/tests/test_add_product.py
+++ bangazon_website/bang_app/tests/test_add_product.py
@@ -7,25 +7,21 @@ from bang_app.models import Product
 
 class TestAddProduct(TestCase):
 
-	'''
-		Purpose:
-			This class tests that a customer can add a product
+    '''
+        Purpose:
+            This class tests that a customer can add a product
 
-		Methods:
-			def test_customer_can_create_product(self):
+        Methods:
+            def test_customer_can_create_product(self):
 
-		Author:
-			@rtwhitfield84
-	'''
+        Author:
+            @rtwhitfield84
+    '''
 
-	def test_customer_can_create_product(self):
+    def test_customer_product_creation_routes_to_products(self):
+        
+        response = self.client.get(reverse('bang_app:products'))
+        self.assertContains(response, "Placeholder")
+        self.assertEqual(response.status_code, 200)
 
-		response = self.client.get(reverse('bang_app:products'))
-		self.assertContains(response, "Placeholder")
-		self.assertEqual(response.status_code, 200)
-
-		# ball = Product("ball", "1.99", "It's round", 3, 1, 1)
-		# response = self.client.post('/create_product/', {'product': ball})
-		# self.assertRedirects(response, '/products/')
-		# self.assertTemplateUsed(response, 'products.html')
-	
+    # def test_customer_can_create_product(self):

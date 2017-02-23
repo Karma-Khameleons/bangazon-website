131b01dec2ff8e65e535802cb4978e39ea847962
Whitney Cormack <cormack.whitney@gmail.com>
2017-02-23 15:05:28 -0600

    initial product detail view and test

---
+++ bangazon_website/bang_app/views/product_detail_view.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from bang_app.models import Product, ProductType
+
+class ProductDetailView(TemplateView):
+  template_name = 'bang_app/product_detail.html'
+
+def list_product_detail(request):
+
+  return HttpResponse('product detail view')
+
+
+
+

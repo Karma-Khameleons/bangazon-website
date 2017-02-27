6ff500fee67117dbc7adb5e492daf48dffc73f1f
LaDonna <boxgirl85@gmail.com>
2017-02-27 10:27:45 -0600

    view, url, and test

--- bangazon_website/bang_app/tests/test_orderSuccess.py
+++ bangazon_website/bang_app/tests/test_orderSuccess.py
@@ -5,14 +5,14 @@ sys.path.append('../')
 
 
 
-class TestSuccessPage(TestCase):
+class TestOrderSuccessPage(TestCase):
 	''' This will test whether the 200 Status response is returned from the expected page, which indicates that
 		the proper page is being rendered. 
 		Author: L.Sales, Karma Khameleons
 	'''
-	def test_display_success_page_after_product_add(self):
-		response = self.client.get(reverse('bang_app:success'))
-		self.assertTemplateUsed('success.html')
+	def test_display_success_page_after_order_complete(self):
+		response = self.client.get(reverse('bang_app:order_success'))
+		self.assertTemplateUsed('order_success.html')
 		self.assertEqual(response.status_code, 200)
 
 

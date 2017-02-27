32b8987cef1d52f02aee1e3341ad0a24da799be6
Whitney Cormack <cormack.whitney@gmail.com>
2017-02-27 10:26:02 -0600

    product detail view in progress

--- bangazon_website/bang_app/views/product_detail_view.py
+++ bangazon_website/bang_app/views/product_detail_view.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponse, HttpResponseRedirect
-from bang_app.models import Product, ProductType
+from bang_app.models import Customer, Product, ProductType
+from bang_app import models
 
-class ProductDetailView(TemplateView):
+class ProductDetailView(ListView):
   template_name = 'product_detail.html'
+  model = models.Product
 
-def list_product_detail(request):
-  return HttpResponse('product detail view')
+  def list_product_detail(request):
+    return HttpResponse('product detail view')
 
+
+  def get_product_details(self, request):
+    self.product_list = Product.objects.all()
+    return render(request, self.template_name, {'product_detail': self.product_detail})

4a571bfb02a77d88b94ad04e7a36be9ec417a382
Samuel Phillips <samphillips1879@gmail.com>
2017-02-27 15:38:51 -0600

    catch for users with no active order

--- bangazon_website/bang_app/views/order_view.py
+++ bangazon_website/bang_app/views/order_view.py
@@ -56,38 +56,11 @@ class OrderDetailView(TemplateView):
 
         except CustomerOrder.DoesNotExist:
             print("order does not exist")
+            self.active_order = CustomerOrder(active_order=1, customer=Customer.objects.get(user=request.user))
+            self.active_order.save()
             # catch for requests from users with no currently active order
             pass
 
-            # # *****************************************
-            # # temporary hard-coding until "add product to order" functionality complete
-            # # when that functionality is available, remove everything in between the lines of asterisks, and uncomment "pass" above
-            # customer = Customer.objects.get(user=request.user)
-            # # self.payment_options = PaymentType(customer_id=customer.id)
-            # payment = PaymentType(None, "Visa", "11111111", "111", "01/11", 
-            # "Happy Gilmore", customer.id)
-            # payment.save()
-            # self.payment_options = PaymentType.objects.filter(customer_id=customer.id)
-
-            # slinky = Product(None, "Slinky", 3.50, "Rusty spring thing", 4, 1, 1)
-            # slinky.save()
-            # ball = Product(None, "Ball", 10, "Bouncy Ball", 10, 1, 1)
-            # ball.save()
-
-
-            # self.active_order = CustomerOrder(active_order=1, customer=customer, payment_type = self.payment_options[0])
-            # self.active_order.save()
-
-            # self.payment_types = PaymentType.objects.filter(customer_id=customer.id)
-
-            # self.active_order.line_items = [slinky, ball]
-            # self.active_order.save()
-
-            # self.line_items=[slinky, ball]
-            # # self.line_items=self.active_order.line_items
-            # for item in self.line_items:
-            #     self.order_total += item.price
-            # # *****************************************
 
 
 

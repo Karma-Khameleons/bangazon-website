edc0d6d8b38e88a8bcc4b398383759ffcc919aca
Rico <richardthomaswhitfield@gmail.com>
2017-03-02 10:43:43 -0600

    removed jquery used for hiding form classes

--- bangazon_website/bang_app/static/main.js
+++ bangazon_website/bang_app/static/main.js
@@ -1,24 +1,3 @@
-/***************************************
-Jquery used to listen for select change
-event, show or hide the proper product
-creation form and send the correct 
-ProductType id value
-****************************************/
-$('#cat-list').on('change', function() {
-	if (this.value == 'new') {
-		$('#newCat').removeClass('hidden');
-		$('#cat-list').addClass('hidden');
-		$('#cats').addClass('hidden');
-	}
-	else {
-		$('#product_type_id').val(this.value);
-		console.log("id", this.value);
-		$('#oldCat').removeClass('hidden');
-		$('#cat-list').addClass('hidden');
-		$('#cats').addClass('hidden');
-	}
-});
-
 // event listener for presenting the payment type creation form on 
 // the order detail view
 $(".payment_type_option").on('click', function() {

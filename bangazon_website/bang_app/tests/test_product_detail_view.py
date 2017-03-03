d5fadeb205a84b43bc0c9ccb6a020b8f2736aeff
Samuel Phillips <samphillips1879@gmail.com>
2017-03-03 11:04:54 -0600

    test_product_detail_routes_to_product reconfigured for new Product Detail template

--- bangazon_website/bang_app/tests/test_product_detail_view.py
+++ bangazon_website/bang_app/tests/test_product_detail_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.db import models
-from bang_app.models import Product, ProductType
+from django.contrib.auth.models import User
+from bang_app.models import Product, ProductType, Customer
 from django.urls import reverse
 
 
@@ -24,9 +25,40 @@ class TestProductDetailView(TestCase):
 
   def test_product_detail_routes_to_product(self):
 
+    category = ProductType.objects.create(
+        label='Toys'
+    )
+
+    user = User(
+        first_name = "Suzy",
+        last_name = "Bishop",
+        email = "s@s.com",
+        username = "suzybishop",
+        password="password1234"
+    )
+    user.save()
+
+    suzy = Customer(
+        user = user,
+        city = "New Penzance" ,
+        state = "Rhode Island" ,
+        zip_code = "52801",
+        street_address = "300 Summer's End" 
+    )
+    suzy.save()
+
+    product = Product.objects.create(
+        name="Slinky",
+        price=6,
+        description="Rusty springy thingy",
+        quantity=5,
+        product_type=category,
+        seller=suzy
+    )
+
     try:
       response = self.client.get(reverse('bang_app:product_detail', args=[1]))
-      self.assertContains(response, "Product Details")
+      self.assertContains(response, "Slinky")
       self.assertEqual(response.status_code, 200)
     except AttributeError:
       pass
